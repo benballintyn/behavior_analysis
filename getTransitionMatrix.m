76b1254113e883e30de624d59f1a7d9ca7448b66
benballintyn <bbal@brandeis.edu>
2019-08-13 11:58:45 -0400

    much faster optimization

--- getTransitionMatrix.m
+++ getTransitionMatrix.m
@@ -2,14 +2,16 @@ function [T] = getTransitionMatrix(stateSeqs,nLocations)
 nDays = size(stateSeqs,1);
 ntimepoints = size(stateSeqs,2);
 T = zeros(nDays,nLocations,nLocations);
-nT = zeros(nDays,nLocations,Nlocations);
+nT = zeros(nDays,nLocations,nLocations);
 for i=1:nDays
     for t=1:ntimepoints-1
         curState = stateSeqs(i,t);
         nextState = stateSeqs(i,t+1);
         nT(i,nextState,curState) = nT(i,nextState,curState)+1;
     end
-    T(i,:,:) = nT(i,:,:)/(ntimepoints-1);
+    for s=1:nLocations
+        T(i,:,s) = squeeze(nT(i,:,s))./sum(squeeze(nT(i,:,s)));
+    end
 end
 end
 
