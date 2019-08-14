b6685a03a4a583c8ef1edae7fc7067588275a1b8
benballintyn <bbal@brandeis.edu>
2019-08-13 21:49:39 -0400

    better code

--- analzyeParamSlices.m
+++ analzyeParamSlices.m
@@ -1,4 +1,4 @@
-function [] = analzyeParamSlices(basedir,modelDir,subdir)
+function [paramVals,paramNames,scores] = analzyeParamSlices(basedir,modelDir,subdir)
 [paramVals,paramNames,scores] = getParamVals(basedir,modelDir,subdir);
 info = load([basedir '/' modelDir '/' subdir '/' modelDir '.mat']); info=info.(modelDir);
 for i=1:length(paramNames)
@@ -14,12 +14,23 @@ for i=1:length(paramNames)
             [Xq,Yq] = meshgrid(xmin:dx:xmax,ymin:dy:ymax);
             Vq = F(Xq,Yq);
             figure;
-            imagesc([xmin xmax],[ymin ymax],Vq); colormap(jet); colorbar()
+            imagesc([xmin xmax],[ymin ymax],Vq); colormap(jet); colorbar(); caxis([0 ceil(max(scores))])
             set(gca,'ydir','normal')
             xlabel(paramNames{i}); ylabel(paramNames{j})
             hold on; plot(paramVals(:,i),paramVals(:,j),'.k','markersize',10)
         end
     end
 end
+[COEFF, SCORE, LATENT, TSQUARED, EXPLAINED, MU] = pca(paramVals);
+cmap=jet;
+colorScale = ceil(max(scores))/size(cmap,1);
+colorInds = ceil(scores/colorScale);
+figure;
+scatter3(paramVals*COEFF(:,1),paramVals*COEFF(:,2),paramVals*COEFF(:,3),[],cmap(colorInds,:),'filled')
+xlabel('PC 1'); ylabel('PC 2'); zlabel('PC 3')
+figure;
+plot(EXPLAINED)
+title('Variance explained')
+
 end
 
--- getParamVals.m
+++ getParamVals.m
@@ -1,14 +1,21 @@
-function [paramVals,paramNames,scores] = getParamVals(basedir,modelDir,subdir)
+function [paramVals,paramNames,scores,subscores] = getParamVals(basedir,modelDir,subdir)
 datadir = [basedir '/' modelDir '/' subdir];
 files=dir(datadir);
 count=0;
 for i=1:length(files)
     if (~(strcmp(files(i).name,'.') || strcmp(files(i).name,'..')) && files(i).isdir)
         count=count+1;
-        p = load([datadir '/' num2str(count) '/params.mat']); p=p.params;
-        paramVals(count,:) = p;
-        score = load([datadir '/' num2str(count) '/score.mat']); score=score.score;
-        scores(count) = score;
+        if (exist([datadir '/' num2str(count) '/score.mat'],'file'))
+            p = load([datadir '/' num2str(count) '/params.mat']); p=p.params;
+            paramVals(count,:) = p;
+            score = load([datadir '/' num2str(count) '/score.mat']); score=score.score;
+            scores(count) = score;
+            ss = load([datadir '/' num2str(count) '/subscores.mat']); ss=ss.subscores;
+            subscores(count,:) =  ss;
+        else
+            count=count-1;
+            continue;
+        end
     end
 end
 
